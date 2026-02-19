2f80456b20414ae1250453672340001e12645881
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2026-02-19 20:58:55 +0000

    Showing the firt cube

--- three.jsPractice/src/App.jsx
+++ three.jsPractice/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import * as THREE from 'three';
 import './App.css'
+import {OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
 
 function App() {
  useEffect(()=>{
@@ -32,7 +34,25 @@ function App() {
   spotLight.position.set(0,64,32);
   scene.add(spotLight);
 
+  const boxGeometry = new THREE.BoxGeometry(16,16,16);
+  const boxMaterial = new THREE.MeshNormalMaterial();
+  const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+  scene.add(boxMesh);
+
+  //Add orbit 
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  // Add FPS status
+
+  const stats = Stats();
+  document.body.appendChild(stats.dom);
+
   const animate = () =>{
+    //boxMesh.rotation.x += 0.01;
+   // boxMesh.rotation.y +=0.01;
+   stats.update();
+   controls.update();
     renderer.render(scene, camera);
     window.requestAnimationFrame(animate);
   };
